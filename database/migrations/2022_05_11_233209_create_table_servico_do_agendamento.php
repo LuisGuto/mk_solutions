dd278de868fe21bbfbf73785e579d9e6c24cf10d
Guilherme <guisena_colorado@hotmail.com>
2022-05-11 20:44:06 -0300

    guilherme

---
+++ database/migrations/2022_05_11_233209_create_table_servico_do_agendamento.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableServicoDoAgendamento extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_servico_do_agendamento', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('agendamento_id')->constrained('agendamento');
+            $table->foreignId('servicos_id')->constrained('servicos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_servico_do_agendamento');
+    }
+}
