484c89c6b31ea86f15c80758ebf9e79f0c11869d
Luis_Guto <luisaugustobotoni@gmail.com>
2022-05-11 20:41:19 -0300

    agendamneto

---
+++ database/migrations/2022_05_11_233149_create_table_agendameto.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableAgendameto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_agendameto', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_cliente')->constrained('cliente');
+            $table->foreignId('id_tecnico')->constrained('tecnico');
+            $table->foreignId('id_atendentes')->constrained('atendentes');
+            $table->foreignId('id_servicos')->constrained('servicos');
+            $table->timestamp('data_hora_prevista');
+            $table->timestamp('data_hora_realizada');
+            $table->integer('status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_agendameto');
+    }
+}
