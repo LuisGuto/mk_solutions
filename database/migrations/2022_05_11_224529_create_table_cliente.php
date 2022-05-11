eba957bf1af275f993796562c0e9ce4f26b506ac
Luis_Guto <luisaugustobotoni@gmail.com>
2022-05-11 20:20:14 -0300

    cliente

---
+++ database/migrations/2022_05_11_224529_create_table_cliente.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableCliente extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 255);
+            $table->string('cpf', 255);
+            $table->string('endereco', 255);
+            $table->string('contato', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+}
