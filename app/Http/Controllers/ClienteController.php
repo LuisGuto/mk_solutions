07b1f7e930063625eb195e0224fe81a944758843
Luis_Guto <luisaugustobotoni@gmail.com>
2022-07-13 20:19:19 -0300

    aa

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cliente;
+
+class ClienteController extends Controller
+{
+    public function lista() {
+
+        $lista = Cliente::all();
+        return view('cliente', ['lista' => $lista]);
+    }
+
+    public function add() {
+        return view('cliente-add', ['cliente' => new Cliente()]);
+    }
+
+    public function edit($id) {
+        $cliente = Cliente::find($id);
+        return view('cliente-add', ['cliente' => $cliente]);
+    }
+
+    public function save(Request $request, $id = null) {
+        $salvou = false;
+        if (empty($id)) {
+            // Se não recebeu um id, então deve criar um novo registro
+            $salvou = Cliente::create($request->all());
+        } else {
+            // Caso contrário, deverá atualizar o registro do id obtido
+            $salvou = Cliente::where('id', $id)->update($request->except('_token', '_method'));
+        }
+        if ($salvou) {
+            return $this->lista();
+        }
+        return view('cliente-form', ['erro' => true]);
+    }
+
+    public function delete($id) {
+        $excluiu = Cliente::where('id', $id)->delete();
+        if ($excluiu) {
+            return $this->lista();
+        }
+        return view('cliente', ['erro' => true]);
+    }
+
+
+
+
+}
