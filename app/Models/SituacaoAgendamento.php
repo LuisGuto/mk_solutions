e0fbf3b067bcc1410e1f6a0f36ffe52955cf970e
Luis <luisaugustobotoni@gmail.com>
2022-07-20 19:08:21 -0300

    attt

--- app/Models/SituacaoAgendamento.php
+++ app/Models/SituacaoAgendamento.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class SituacaoAgendamento extends Model
 {
     use HasFactory;
+    
+
 
     protected $table = 'situacao_agendamento';
     protected $primarykey = "id-situacaoAgendamento";
