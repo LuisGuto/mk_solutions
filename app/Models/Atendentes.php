542978841effbc8d231db0c32507a4f2d23aceb5
Luis <luisaugustobotoni@gmail.com>
2022-07-20 21:24:17 -0300

    22

--- app/Models/Atendentes.php
+++ app/Models/Atendentes.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Atendentes extends Model
+class Atendentes extends Model implements Auditable
 {
     use HasFactory;
+    use \OwenIt\Auditing\Auditable;
 
-    protected $table = 'Atendentes';
+    protected $table = 'atendentes';
 
     protected $fillable = [
         'nome',
