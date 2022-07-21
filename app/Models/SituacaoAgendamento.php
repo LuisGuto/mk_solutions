78068e60924ae6bfb6e1373c69936f81af2bc1bb
Luis <luisaugustobotoni@gmail.com>
2022-07-20 21:26:05 -0300

    2w1231

--- app/Models/SituacaoAgendamento.php
+++ app/Models/SituacaoAgendamento.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-
-class SituacaoAgendamento extends Model
+class SituacaoAgendamento extends Model implements Auditable
 {
     use HasFactory;
-    
+    use \OwenIt\Auditing\Auditable;
 
 
     protected $table = 'situacao_agendamento';
