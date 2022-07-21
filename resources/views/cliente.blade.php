414a042f30c3a12e6532f0fc92bfbeb502f931ab
Luis <luisaugustobotoni@gmail.com>
2022-07-20 21:26:40 -0300

    wq231

--- resources/views/cliente.blade.php
+++ resources/views/cliente.blade.php
@@ -27,6 +27,7 @@
             <table id="example1" class="table table-bordered table-striped">
                 <thead>
                     <tr>
+                        <th>Ações</th>
                         <th>ID</th>
                         <th>Nome</th>
                         <th>Cpf</th>
