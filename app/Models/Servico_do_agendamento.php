2dc83131ee94e64a2978257ec8f20818e75b6e62
Luis <luisaugustobotoni@gmail.com>
2022-07-20 21:25:40 -0300

    1212

--- app/Models/Servico_do_agendamento.php
+++ app/Models/Servico_do_agendamento.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Servico_do_agendamento extends Model
+class Servico_do_agendamento extends Model implements Auditable
 {
     use HasFactory;
+    use \OwenIt\Auditing\Auditable;
 
-    protected $table = 'Servico_do_agendamento';
+    protected $table = 'servico_do_agendamento';
     protected $primarykey = "id_servico_do_agendamento";
 
     protected $fillable = [
